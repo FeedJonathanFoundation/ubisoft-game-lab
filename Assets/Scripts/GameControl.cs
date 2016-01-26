71a00597dedff9a36bd66e5aa5fe2a5a7527597d
Alex Ilea <alex@alisterdev.com>
2016-01-25 23:36:36 -0500

    - Implemented save functionality - Integrated Parse API to save online - Directory cleanup

--- Assets/Scripts/GameControl.cs
+++ Assets/Scripts/GameControl.cs
@@ -1,15 +1,90 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using Parse;
 
-public class GameControl : MonoBehaviour {
+/// <summary>
+///  This class keeps track of the game in progress.
+/// </summary>
+public class GameControl : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public static GameControl control;
+
+    public float playerLight;
+    public int level;
+
+    
+    /// <summary>
+    ///  Executed before Start()
+    /// Makes sure that there's only 1 instance of the gameObject
+    /// across different scenes.
+    /// </summary>
+    void Awake() 
+    {
+        if (control == null) 
+        {
+            DontDestroyOnLoad(gameObject);
+            control = this;
+        } 
+        else if (control != this) 
+        {
+            Destroy(gameObject);
+        }    
+    }
+    
+    void Start()
+    {
+        this.Save();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    
+    public void Save()
+    {
+        // save locally
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Create(Application.persistentDataPath + ".playerInfo.dat");
+        
+        PlayerData data = new PlayerData();
+        data.playerLight = playerLight;
+        data.level = level;
+        bf.Serialize(file, data);
+        file.Close();
+        
+        // save online
+        ParseObject gameControl = new ParseObject("GameControl");
+        gameControl["playerLight"] = 90;
+        gameControl["level"] = 1;
+        gameControl.SaveAsync();
+        
+    }
+    
+    public void Load()
+    {
+        if (File.Exists(Application.persistentDataPath + "playerInfo.dat")) 
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(Application.persistentDataPath + ".playerInfo.dat", FileMode.Open);
+            PlayerData data = (PlayerData) bf.Deserialize(file);
+            file.Close();
+            
+            playerLight = data.playerLight; 
+            level = data.level;          
+        }
+    }
+    
+}
+
+[Serializable]
+class PlayerData
+{
+    public float playerLight;
+    public int level;
 }
