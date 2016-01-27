533fa21d0dfe2e66e6ce0c52f4d8bc01e84b3ed0
user <jonathan.lucuix@gmail.com>
2016-01-26 19:59:40 -0500

    Preliminary Thrusting Working

--- Assets/Scripts/Character/PlayerMovement.cs
+++ Assets/Scripts/Character/PlayerMovement.cs
@@ -8,6 +8,11 @@ public class PlayerMovement : MonoBehaviour
     /// The position at which the character ejects mass. 
     /// </summary> 
     public Transform massEjectionTransform;
+   
+    /// <summary>
+    /// The light ball ejected by the player when thrusting
+    /// </summary>
+    public GameObject lightBallPrefab;
     
     /// <summary>
     /// The amount of force applied on the player when ejecting one piece of mass.
@@ -15,11 +20,13 @@ public class PlayerMovement : MonoBehaviour
     public float thrustForce;
 
     /** Caches the player's components */
+    private new Transform transform;
     private new Rigidbody rigidbody;
 
 	// Use this for initialization
 	void Start () 
     {
+       transform = GetComponent<Transform>();
 	   rigidbody = GetComponent<Rigidbody>();
 	}
 	
@@ -28,24 +35,41 @@ public class PlayerMovement : MonoBehaviour
     {
 	   if(Input.GetButtonDown("Thrust"))
        {
-           
+           // Eject mass in the direction of the left stick
+           EjectMass(InputManager.GetLeftStick());
        }
        
+       // Makes the character follow the left stick's rotation
+       FollowLeftStickRotation();
        
-           
-       Debug.Log("Eject Mass in direction: " + InputManager.GetLeftStick());
-
-       
-
+       //Debug.Log("Eject Mass in direction: " + InputManager.GetLeftStick());
 	}
+    
+    /// <summary>
+    /// Makes the character follow the left stick's rotation.
+    /// </summary>
+    private void FollowLeftStickRotation()
+    {
+        // Get the direction the left stick is pointing to
+        Vector2 leftStickDirection = InputManager.GetLeftStick();
+        
+        // Get the angle the left stick is pointing in
+        float leftStickAngle = 0.0f;
+        if(leftStickDirection.x != 0 || leftStickDirection.y != 0)
+        {
+            // 90-degree offset to ensure angle is relative to +y-axis
+            leftStickAngle = Mathf.Atan2(leftStickDirection.y,leftStickDirection.x) * Mathf.Rad2Deg - 90;
+        }
+        
+        // Make the character face in the direction of the left stick
+        transform.localEulerAngles = new Vector3(0,0,leftStickAngle);
+    }
     
     /// <summary>
     /// Ejects mass in the given direction, pushing the gameObject in the opposite direction.
     /// </summary>
     private void EjectMass(Vector2 direction)
     {
-        
-        
         // Push the character in the opposite direction that the mass was ejected
         rigidbody.AddForce(-thrustForce * direction, ForceMode.Impulse);
     }
