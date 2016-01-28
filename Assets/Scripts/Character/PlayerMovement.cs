f06637913180e47f18ff73d7d725393964af8f92
Alex Ilea <alex@alisterdev.com>
2016-01-28 11:38:35 -0500

    some updats

--- Assets/Scripts/Character/PlayerMovement.cs
+++ Assets/Scripts/Character/PlayerMovement.cs
@@ -33,7 +33,7 @@ public class PlayerMovement : MonoBehaviour
 	// Update is called once per frame
     void Update () 
     {
-        if (Input.GetButtonDown("Thrust") || Input.GetKeyDown("space"))
+        if (Input.GetButtonDown("Thrust"))
         {
             // Eject mass in the direction of the left stick
             EjectMass(massEjectionTransform.up);
