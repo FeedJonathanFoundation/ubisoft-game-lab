e6359be024980a77317b8caa777d826cfa517858
stellarxo <stellajhlee@hotmail.com>
2016-01-30 13:48:19 -0500

    add npcaction

---
+++ Assets/Scripts/NPC/NPCAction.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NPCAction : MonoBehaviour 
+{
+    private NPCActionable executableStrategy;
+    
+    // Assigns strategy to interface
+    public NPCAction(NPCActionable strategy)
+    {
+        this.executableStrategy = strategy;
+    }
+
+    // Executes the strategy
+    public void Execute(int speed)
+    {
+        executableStrategy.Execute(speed);
+    }
+}
+
