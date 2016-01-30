dd714d5618ba197681a7dae091162414b32dc94f
TheFunVacuum <simonthompson@hotmail.ca>
2016-01-29 20:10:44 -0600

    Added basic light toggling

---
+++ Assets/Scripts/character/LightControl.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightControl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if (Input.GetKeyDown("f")) {
+			if (GetComponent<Light>().enabled)
+            {
+				ToggleLights(false);
+			}
+			else 
+            {
+				ToggleLights(true);
+			}
+		}
+	}
+    
+    public void ToggleLights (bool toggle)
+    {
+        foreach (Behaviour childComponent in GetComponent<Light>().GetComponentsInChildren<Light>()) 
+        {
+            childComponent.enabled = toggle;
+        }
+    }
+}
