fef2b13ce24fbfe7b1326ce78683efe2641491ad
stellarxo <stellajhlee@hotmail.com>
2016-01-31 20:48:10 -0500

    school class

---
+++ Assets/Scripts/Fish/School.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class School : AbstractFish 
+{
+    private AbstractFish[] fishes;
+    public int schoolPopulation;
+    public int minSchoolPopulation;         // Minimum number of fish in one school
+    public int maxSchoolPopulation;         // Maximum number of fish in one school
+    
+    public School()
+    {
+        schoolPopulation = Random.Range(minSchoolPopulation, maxSchoolPopulation);
+        fishes = new AbstractFish[schoolPopulation];
+    }
+    
+   public override void Move()
+   {
+       foreach (AbstractFish fish in fishes)
+       {
+           // fish.doThis();
+           fish.Move();
+       }
+   }
+    
+    // How the fish moves when it is proximate to the player
+    public override void ReactToPlayer(Transform player)
+    {
+        // something
+    }
+    
+    // How the fish moves when it is proximate to the player
+    public override void ReactToNPC(Transform other) 
+    {
+        // here
+    }
+    
+}
