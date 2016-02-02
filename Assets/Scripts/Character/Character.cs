3c643fb0173cc8ef2233f11b36089253e1670c1b
Alex Ilea <alex@alisterdev.com>
2016-02-02 00:08:38 -0500

    attempt to correct identation

--- Assets/Scripts/Character/Character.cs
+++ Assets/Scripts/Character/Character.cs
@@ -2,49 +2,48 @@ using UnityEngine;
 using System.Collections;
 
 [RequireComponent(typeof(Rigidbody))]
-public class Character : MonoBehaviour 
+public class Character : MonoBehaviour
 {
 
     /// <summary>
-    /// The position at which the character ejects mass. 
-    /// </summary> 
+    /// The position at which the character ejects mass.
+    /// </summary>
     public Transform massEjectionTransform;
-   
+
     /// <summary>
     /// The light ball ejected by the player when thrusting
     /// </summary>
     public GameObject lightBallPrefab;
-    
+
     /// <summary>
     /// The amount of force applied on the player when ejecting one piece of mass.
     /// </summary>
     public float thrustForce;
 
-    // Private vars 
+    // Private vars
     private PlayerMovement movement;
     private Transform transform;
     private Rigidbody rigidbody;
 
-
-	// Use this for initialization
-	void Awake() 
-    {       
+    // Use this for initialization
+    void Awake()
+    {
        transform = GetComponent<Transform>();
-       rigidbody = GetComponent<Rigidbody>();      	        
-       movement = new PlayerMovement(massEjectionTransform, lightBallPrefab, thrustForce, transform, rigidbody);        
-	}
-    
+       rigidbody = GetComponent<Rigidbody>();
+       movement = new PlayerMovement(massEjectionTransform, lightBallPrefab, thrustForce, transform, rigidbody);
+    }
+
     void Start()
     {
-        
+
     }
-	
-	// Update is called once per frame
-	void Update()
+
+    // Update is called once per frame
+    void Update()
     {
-		Move();
-	}
-    
+        Move();
+    }
+
     /// <summary>
     /// Player movements responding to user input
     /// </summary>
@@ -55,7 +54,7 @@ public class Character : MonoBehaviour
             // Eject mass in the direction of the left stick
             movement.EjectMass(massEjectionTransform.up);
         }
-               
+
         // Makes the character follow the left stick's rotation
         movement.FollowLeftStickRotation();
     }
