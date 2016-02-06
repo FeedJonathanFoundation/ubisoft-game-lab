d1f37523fcd3aea72c39e47bb5417c3a5f0dc326
stellarxo <stellajhlee@hotmail.com>
2016-02-06 09:59:23 -0500

    working spawn volume

--- Assets/Scripts/NPC/SpawnVolume.cs
+++ Assets/Scripts/NPC/SpawnVolume.cs
@@ -1,124 +1,170 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class SpawningVolume : MonoBehaviour
-{
-
-    public AbstractFish[] fishesToSpawn;        // Fish prefabs to be spawned
-    public float spawnTime = 3f;                // Time between spawns
-    public bool overrideSpawnLocations = false; // Determines usage of random or specific spawn points
+public class SpawnVolume : MonoBehaviour {
+    
+    /// <summary>
+    /// Delay between spawns
+    /// </summary>
+    [Tooltip("Delay between spawns")]
+    public float spawnTime;
+    
+    /// <summary>
+    /// Array of possible NPCs to spawn
+    /// </summary>
+    [Tooltip("Array of possible NPCs to spawn")]
+    public GameObject[] spawnObject;
+    
+    /// <summary>
+    /// If true, uses specific spawn points.
+    /// Else, uses randomly generated spawn points.
+    /// </summary>
+    [Tooltip("If true, uses specific spawn points. Else, uses randomly generated spawn points.")]
+    public bool overrideSpawnLocations = false;
+    /// <summary>
+    /// Specific spawn points.
+    /// Only used if overrideSpawnLocations == true
+    /// </summary>
+    [Tooltip("Specific spawn points.")]
     public Transform[] spawnPoints;             // Array of possible spawn points
-    public float spawnPointRadius;              // Radius of sphere to check for spawned fish
+    
+    /// <summary>
+    /// Radius of sphere to check for spawned fish.
+    /// </summary>
+    [Tooltip("Radius of sphere to check for spawned fish.")]
+    public float spawnPointRadius;
+
+    
+    /// <summary>
+    /// Minimum number of fish to be spawned.
+    /// </summary>
+    [Tooltip("Minimum number of fish to be spawned.")]
+    public int minFishCount;
+    /// <summary>
+    /// Maximum number of fish to be spawned.
+    /// </summary>
+    [Tooltip("Maximum number of fish to be spawned.")]
+    public int maxFishCount;
+    
+    /// <summary>
+    /// Tracks number of fish spawned.
+    /// </summary>
+    private int fishCount;
+    
     public int minFishDifficulty;               // Mass of smallest fish
     public int maxFishDifficulty;               // Mass of largest fish
-    public int minFishCount;                    // Minimum number of fish in level
-    public int maxFishCount;                    // Maximum number of fish in level
 
-    private int fishCount;
-
-    void Start()
+    /// <summary>
+    /// Initializes fish count to 0,
+    /// Spawns the minimum number of fish without delay
+    /// Spawns the maximum number of fish incrementally
+    /// </summary>
+	void Start () 
     {
         fishCount = 0;
-
-        // Spawn minimum number of fish
-        while (fishCount < minFishCount)
+        for (int i = 0; i < minFishCount; i++)
         {
-            InvokeRepeating("Spawn", 0, 0);
-        }
+            Invoke("Spawn", 0f);                            // Spawn minimum number of fish
+        }     
+        InvokeRepeating ("Spawn", spawnTime, spawnTime);    // Spawn after a delay of spawnTime
+	}
 
-       // Spawn after a delay of spawnTime and continue to call after the same amount of time
-       while (fishCount < maxFishCount)
-       {
-           InvokeRepeating ("Spawn", spawnTime, spawnTime);
-       }
-    }
-
+    /// <summary>
+    /// If player is dead or max number of fish have been spawned,
+    /// exit function
+    /// If spawn location is valid, spawn fish
+    /// </summary>
     void Spawn()
     {
         // if (player.isDead) { return; // exit function }
+        if (fishCount >= maxFishCount) { return; }
 
         Vector3 spawnLocation;
-
-        // Choose random fish type to spawn
-        int spawnTypeIndex = Random.Range(0, fishesToSpawn.Length);
+        int spawnTypeIndex = ChooseFish();
 
         // Use specific spawn points
         // else use random spawn point
-        if (overrideSpawnLocations)
-        {
-            int spawnPointIndex = Random.Range(0, spawnPoints.Length);
-            spawnLocation = spawnPoints[spawnPointIndex].position;
-        }
-        else
-        {
+        // if (overrideSpawnLocations)
+        // {
+        //     int spawnPointIndex = Random.Range(0, spawnPoints.Length);
+        //     spawnLocation = spawnPoints[spawnPointIndex].position;
+        // }
+        // else
+        // {
             // generate random point in sphere collider
-            spawnLocation = Random.insideUnitSphere * GetRadius();
-        }
+            spawnLocation = Random.insideUnitSphere * GetSpawnVolumeRadius();
+            while (!IsValidSpawnPoint(spawnLocation))
+            {
+            spawnLocation = Random.insideUnitSphere * GetSpawnVolumeRadius();
+            }
+        // }
 
         // If spawn point is not occupied, spawn fish
         if (IsValidSpawnPoint(spawnLocation))
         {
             // Create instance of fish prefab at spawn point and rotation
-            Instantiate(fishesToSpawn[spawnTypeIndex], spawnLocation, Quaternion.identity);
+            Instantiate(spawnObject[spawnTypeIndex], spawnLocation, Quaternion.identity);
             fishCount++;
         }
     }
-
+    
     void SpawnSchool()
     {
-        // if (player.isDead) { return; // exit function }
+        // generate
+                // if (player.isDead) { return; // exit function }
 
-        Vector3 spawnLocation;
-        // Choose random index within number of spawn points
-        int spawnPointIndex = Random.Range(0, spawnPoints.Length);
+        // Vector3 spawnLocation;
+        // // Choose random index within number of spawn points
+        // int spawnPointIndex = Random.Range(0, spawnPoints.Length);
 
-        // Use specific spawn points
-        if (overrideSpawnLocations)
-        {
-            spawnLocation = spawnPoints[spawnPointIndex].position;
-        }
-        // Use random spawn point
-        else
-        {
-            // generate random point in sphere collider
-            spawnLocation = Random.insideUnitSphere * GetRadius();
-        }
+        // // Use specific spawn points
+        // if (overrideSpawnLocations)
+        // {
+        //     spawnLocation = spawnPoints[spawnPointIndex].position;
+        // }
+        // // Use random spawn point
+        // else
+        // {
+        //     // generate random point in sphere collider
+        //     spawnLocation = Random.insideUnitSphere * GetRadius();
+        // }
 
-        // Choose random fish type to spawn
-        int spawnTypeIndex = Random.Range(0, fishesToSpawn.Length);
+        // // Choose random fish type to spawn
+        // int spawnTypeIndex = Random.Range(0, fishesToSpawn.Length);
 
-        School newSchool = new School(fishesToSpawn[spawnTypeIndex]);
-        int schoolPopulation = newSchool.GetSchoolPopulation();
+        // School newSchool = new School(fishesToSpawn[spawnTypeIndex]);
+        // int schoolPopulation = newSchool.GetSchoolPopulation();
 
-        // If spawn point is not occupied, spawn fish
-        if (IsValidSpawnPoint(spawnLocation))
-        {
-            // Create instance of fish prefab at spawn point and rotation
-            for (int i = 0; i < schoolPopulation; i++)
-            {
-                // Calculate school pattern
-                // Vector3 pos = spawnLocation + spacing * i;  // NEEDS TO BE FIXED
-                // Instantiate(fishesToSpawn[spawnTypeIndex], pos, Quaternion.identity);
-                fishCount++;
-            }
-        }
+        // // If spawn point is not occupied, spawn fish
+        // if (IsValidSpawnPoint(spawnLocation))
+        // {
+        //     // Create instance of fish prefab at spawn point and rotation
+        //     for (int i = 0; i < schoolPopulation; i++)
+        //     {
+        //         // Calculate school pattern
+        //         // Vector3 pos = spawnLocation + spacing * i;  // NEEDS TO BE FIXED
+        //         // Instantiate(fishesToSpawn[spawnTypeIndex], pos, Quaternion.identity);
+        //         // fishCount++;
+        //     }
+        // }
     }
-
-    // Checks whether the spawn point is occupied within the radius
-    bool IsOccupied(Vector3 spawnPoint, float spawnPointRadius)
+    
+    int ChooseFish()
     {
-        // Array of all colliders touching or inside sphere
-        Collider[] hitColliders = Physics.OverlapSphere(spawnPoint, spawnPointRadius);
-        if (hitColliders.Length < 1) { return false; }
-        return true;
+        // Choose random fish type to spawn
+        return Random.Range(0, spawnObject.Length);
+        
+        // Choose specific fish to spawn
+        // Make flag for specific fish?
     }
-
-
+    
     bool IsValidSpawnPoint(Vector3 spawnPoint)
     {
         // Calculate spawn radius
-        int spawnTypeIndex = Random.Range(0, fishesToSpawn.Length);
-        float spawnRadius = fishesToSpawn[spawnTypeIndex].CalculateRadius();
+        // int spawnTypeIndex = Random.Range(0, fishesToSpawn.Length);
+        // float spawnRadius = spawnObject.GetHeight() / 2;
+        
+        float spawnRadius = 1f;
 
         if (IsOccupied(spawnPoint, spawnRadius))
         {
@@ -126,11 +172,40 @@ public class SpawningVolume : MonoBehaviour
         }
         return true;
     }
-
-    float GetRadius()
+    
+    // UNDER CONSTRUCTION
+    // Checks whether the spawn point is occupied within the radius
+    bool IsOccupied(Vector3 spawnPoint, float spawnRadius)
+    {
+        // Array of all colliders touching or inside sphere
+        Collider[] hitColliders = Physics.OverlapSphere(spawnPoint, spawnRadius);
+        if (hitColliders.Length < 1) { return false; }
+        return true;
+    }
+    
+    public float GetSpawnVolumeRadius()
     {
         float radius = this.GetComponent<SphereCollider>().radius;
         return radius;
     }
-
+    
+    // Following functions used if NPC is added/destroyed
+    public int GetFishCount()
+    {
+        return fishCount;
+    }
+    
+    public void IncrementFishCount()
+    {
+        fishCount++;
+    }
+    
+    public void DecrementFishCount()
+    {
+        fishCount--;
+    }
+    
 }
+
+/// implement probabilities to spawn certain fish types
+
