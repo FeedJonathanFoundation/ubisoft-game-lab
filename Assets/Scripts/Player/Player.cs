e0a8aeeccdf2837781cee7a25440cc9883ccd403
Alex Ilea <alex@alisterdev.com>
2016-02-12 18:34:56 -0500

    refactored LightToggle. It is now part of Player module, as it should be

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -27,8 +27,12 @@ public class Player : LightSource
     [Tooltip("The amount of light energy spent when ejecting one piece of mass.")]
     public float thrustEnergyCost = 1;
 
+    [Tooltip("If true, the lights are enabled on scene start.")]
+    public bool defaultLightStatus = true;
+
     /** Caches the player's components */
     private PlayerMovement movement;
+    private PlayerLightToggle lightToggle;
     private bool isDead; // determines is current player is dead
     private new Transform transform;
     private new Rigidbody rigidbody;
@@ -40,9 +44,12 @@ public class Player : LightSource
        transform = GetComponent<Transform>();
        rigidbody = GetComponent<Rigidbody>();
        movement = new PlayerMovement(massEjectionTransform, lightBallPrefab, thrustForce, thrustEnergyCost, transform, rigidbody, this.LightEnergy);
+       lightToggle = new PlayerLightToggle(transform.Find("LightsToToggle").gameObject, defaultLightStatus);
        this.LightEnergy.LightDepleted += OnLightDepleted;
        this.isDead = false;
        LoadGame();
+
+       Debug.Log(transform.Find("LightsToToggle").gameObject);
     }
 
     void Start()
@@ -60,6 +67,7 @@ public class Player : LightSource
         else
         {
             Move();
+            LightControl();
         }
     }
 
@@ -79,6 +87,14 @@ public class Player : LightSource
 
         // Ensure that the rigidbody never spins
         rigidbody.angularVelocity = Vector3.zero;
+    }
+
+    private void LightControl()
+    {
+        if (Input.GetButtonDown("LightToggle"))
+        {
+            lightToggle.ToggleLights();
+        }
     }
 
     private void Restart()
