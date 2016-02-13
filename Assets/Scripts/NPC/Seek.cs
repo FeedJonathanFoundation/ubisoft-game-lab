fef7795e53e0027f6ccac603c29fc2cc369127de
stellarxo <stellajhlee@hotmail.com>
2016-02-13 14:47:43 -0500

    missing seek files

---
+++ Assets/Scripts/NPC/Seek.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Seek : MonoBehaviour, NPCActionable
+{
+    
+    public Transform targetTransform;
+    
+    public float strengthMultiplier = 9.9f;
+    
+	public void Execute(Steerable steerable) 
+    {
+        targetTransform = GameObject.FindGameObjectWithTag("Player").transform;
+        steerable.AddSeekForce (targetTransform.position, strengthMultiplier);
+    }
+}
