cf0af0bac341b8b4772a7581672b137c09aeb9e5
Alex Ilea <alex@alisterdev.com>
2016-02-14 00:02:43 -0500

    Refactored Fish to extend LightSource + created NPC(Seek) prefab for small fish that can be absorbed

--- Assets/Scripts/Fish/FishA.cs
+++ Assets/Scripts/Fish/FishA.cs
@@ -5,28 +5,57 @@ using System.Collections;
 // Run away upon close contact
 public class FishA : AbstractFish
 {
-    
+
+    // SteeringBehavior steeringBehavior;
+    NPCAction steeringBehavior;
+
     NPCAction reactionToPlayer;
-    
-    public FishA() { }
-    
-    public FishA(NPCAction reactionToPlayer) : base(reactionToPlayer) 
+
+    // public FishA() { }
+
+    // public FishA(NPCAction reactionToPlayer) : base(reactionToPlayer)
+    // {
+    //     this.reactionToPlayer = reactionToPlayer;
+    // }
+    public override void Awake()
     {
-        this.reactionToPlayer = reactionToPlayer;
+        base.Awake(); // call parent LightSource Awake() first
     }
-    
-    public override void Move() 
+
+    public override void Move()
     {
         // movement.Execute(movementSpeed);
+        // if (actionQueue.Count > 0) {
+        //     NPCAction current = actionQueue.Dequeue();
+        //     current.Execute(steerable);
+        // }
+        // else {
+        // actionQueue.Enqueue(new NPCAction(new Wander()));
+        // }
+
+        // NPCAction wander = new NPCAction(new Wander());
+        // wander.Execute(steerable);
+        // // Apply the forces on the steerable that is performing this action
+		// steerable.ApplyForces (Time.fixedDeltaTime);
     }
-    
+
     public override void ReactToPlayer(Transform player)
     {
-        reactionToPlayer.Execute(reactionSpeed);
+        // reactionToPlayer.Execute(steerable);
+        //NPCAction current = new NPCAction(new Seek());
+        //current.Execute(steerable);
+
+        // Apply the forces on the steerable that is performing this action
+		steerable.ApplyForces(Time.fixedDeltaTime);
     }
-    
+
     public override void ReactToNPC(Transform other)
     {
-        reactionToPlayer.Execute(reactionSpeed);
+        // reactionToPlayer.Execute(reactionSpeed);
+        // NPCAction current = new NPCAction(new Seek());
+        // current.Execute(steerable);
+
+        // Apply the forces on the steerable that is performing this action
+		steerable.ApplyForces (Time.fixedDeltaTime);
     }
 }
