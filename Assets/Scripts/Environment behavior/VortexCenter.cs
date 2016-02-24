8d86b9aa8aaa41f45ab19b764e8d9c62ab1f8476
IAmAllOfYou <k.chiraz93@gmail.com>
2016-02-24 18:31:35 -0500

    player isnt pulled if behind objects with a "Wall" tag

---
+++ Assets/Scripts/Environment behavior/VortexCenter.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VortexCenter : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    void OnTriggerEnter(Collider col)
+     {
+         //need to put code to not pull on walls
+         //Debug.Log("In field");
+         if(col.gameObject.tag == "Player")
+         {
+             Player player;
+             player = col.gameObject.GetComponent<Player>();
+             player.LightEnergy.Deplete(player.LightEnergy.CurrentEnergy+10);
+         }
+         
+        /*if(col.transform.tag == "guest")
+             count++;*/
+     }
+}
--- Assets/Scripts/Environment behavior/VortexPull.cs
+++ Assets/Scripts/Environment behavior/VortexPull.cs
@@ -6,7 +6,7 @@ public class VortexPull : MonoBehaviour {
     public float strengthOfAttraction;
     [Tooltip("Increases the roation of elements inside the whirlpool")]
     public float strengthOfRotation;
-    private List<GameObject> objectsInVortex = new List<GameObject>();
+    private List<GameObject> objectsInVortex = new List<GameObject>(); //this might change if the player is the only one attracted to vortex
 	// Use this for initialization
 	void Start () 
     {
@@ -17,37 +17,78 @@ public class VortexPull : MonoBehaviour {
 	void Update () 
     {
         
-        foreach(GameObject objectsAttracted in objectsInVortex)
+        foreach(GameObject objectsAttracted in objectsInVortex.ToArray())
         {
-            Vector3 offset = this.transform.position - objectsAttracted.GetComponent<Rigidbody>().transform.position;
-            offset.z = 0;
-            float magsqr = offset.sqrMagnitude;
-            
-            if (magsqr > 0.0001f)
+            if(rayChecker(objectsAttracted))
             {
-                objectsAttracted.GetComponent<Rigidbody>().AddForce(strengthOfAttraction * offset.normalized / magsqr, ForceMode.Acceleration);
-                objectsAttracted.transform.RotateAround(this.transform.position, Vector3.forward, strengthOfRotation * Time.deltaTime);
+                //this part creates the pull and rotation on the player
+                Vector3 offset = this.transform.position - objectsAttracted.GetComponent<Rigidbody>().transform.position;
+                offset.z = 0;
+                float magsqr = offset.sqrMagnitude;
+                
+                if (magsqr > 0.0001f)
+                {
+                    objectsAttracted.GetComponent<Rigidbody>().AddForce(strengthOfAttraction * offset.normalized / magsqr, ForceMode.Acceleration);
+                    objectsAttracted.transform.RotateAround(this.transform.position, Vector3.forward, strengthOfRotation * Time.deltaTime);
+                }
+            }
+            else
+            {
+                colliderObjectRemover(objectsAttracted);
             }
         }
 	}
     
     void OnTriggerEnter(Collider col)
-     {
-         //need to put code to not pull on walls
-         //Debug.Log("In field");
-         if(col.gameObject.tag == "Player")
-         {
-             objectsInVortex.Add(col.gameObject);
+    {
+        if(gameObject.tag == "Player" && !objectsInVortex.Contains(gameObject))
+        {
+            if(rayChecker(gameObject))
+            {
+                objectsInVortex.Add(gameObject);
+            }
          }
-         
-        /*if(col.transform.tag == "guest")
-             count++;*/
-     }
-     
-     void OnTriggerExit(Collider col) 
-     {
-        // Destroy everything that leaves the trigger
-        //Debug.Log("Out field");
-        objectsInVortex.Remove(col.gameObject);
+    }
+    
+    void OnTriggerExit(Collider col) 
+    {
+        colliderObjectRemover(col.gameObject);
+    }
+    
+    void OnTriggerStay(Collider col)
+    {
+        if(gameObject.tag == "Player" && !objectsInVortex.Contains(gameObject))
+        {
+            if(rayChecker(gameObject))
+            {
+                objectsInVortex.Add(gameObject);
+            }
+         }
+    }
+    
+    bool rayChecker(GameObject gameObject) 
+    {
+        Ray ray = new Ray();
+        RaycastHit hit;
+        ray.origin = this.transform.position;
+        ray.direction = gameObject.transform.position - this.transform.position;
+        //checks if raycast hits a target and if the target is not a wall
+        if(Physics.Raycast(ray, out hit) && hit.transform.tag != "Wall")
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    void colliderObjectRemover(GameObject gameObject)
+    {
+        //removes objects from list
+        if(objectsInVortex != null && objectsInVortex.Contains(gameObject))
+        {
+            objectsInVortex.Remove(gameObject);
+            gameObject.GetComponent<Rigidbody>().angularVelocity = Vector3.zero; //sets the attraction to vortex to 0 but keeps rotation
+        }
     }
 }
