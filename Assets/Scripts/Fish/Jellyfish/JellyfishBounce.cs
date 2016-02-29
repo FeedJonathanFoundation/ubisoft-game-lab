3ea341d521dc1fae2dd265602ef6d37daf7f93b8
IAmAllOfYou <k.chiraz93@gmail.com>
2016-02-29 11:28:43 -0500

    bounce, shock and movement up and down of jellyfish

---
+++ Assets/Scripts/Fish/Jellyfish/JellyfishBounce.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JellyfishBounce : MonoBehaviour 
+{
+    //NOTE IMPORTANT
+    //in order to make this script work and to make the jellyfish not get affected by the collision
+    //make sure is kenematic is checked in rigidbody
+    void OnTrigger(Collision col)
+    {
+        //Check if you have to be specific to an object to bounce
+        //this could not exist and the player would still bounce off the body as long as istrigger is not checked
+        if(col.gameObject.tag == "Player")
+        {
+            col.gameObject.GetComponent<Rigidbody>().velocity = Vector3.Reflect(col.relativeVelocity*-1, col.contacts[0].normal );
+        }
+    }
+}
---
+++ Assets/Scripts/Fish/Jellyfish/JellyfishMovement.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JellyfishMovement : MonoBehaviour 
+{
+    public float jellyfishTravelDistance;
+    public float jellyfishSpeed;
+    private Vector3 jellyfishStart;
+    private bool goingUp;
+    private Vector3 mouvement;
+    private Rigidbody parenRigidbody; //make sure the root parent has a rigidbody!!!
+	// Use this for initialization
+	void Start () 
+    {
+	   jellyfishStart = this.transform.position;
+       goingUp = false;
+       parenRigidbody = this.transform.root.GetComponent<Rigidbody>();
+       SetVolocity();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        float distance = Vector3.Distance(jellyfishStart,this.transform.position);
+        Debug.Log(distance.ToString());
+        if(distance >= jellyfishTravelDistance)
+        {
+            Debug.Log("distance >=");
+            goingUp = !goingUp;
+            SetVolocity();
+        }
+	}
+    
+    void SetVolocity()
+    {
+        if(goingUp)
+        {
+            Debug.Log("up");
+            mouvement = Vector3.up * jellyfishSpeed;
+        }
+        else
+        {
+            Debug.Log("down");
+            mouvement = Vector3.down * jellyfishSpeed;
+        }
+        Debug.Log(mouvement.ToString());
+        parenRigidbody.velocity = Vector3.zero;
+        parenRigidbody.AddForce(mouvement * jellyfishSpeed,ForceMode.Force);
+    }
+}
