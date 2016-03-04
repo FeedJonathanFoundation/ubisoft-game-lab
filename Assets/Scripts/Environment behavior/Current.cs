352ccd3955e6218c1f41e4c27e7f3bc44f75b07b
stellarxo <stellajhlee@hotmail.com>
2016-03-04 13:39:17 -0500

    current boundary

---
+++ Assets/Scripts/Environment behavior/Current.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Current : MonoBehaviour
+{
+    
+    public float strength;
+    private bool empty;
+    private List<Rigidbody> rbs;
+    
+    void Start()
+    {
+        rbs = new List<Rigidbody>();
+        empty = true;
+    }
+    
+    void Update()
+    {
+        if (!empty)
+        {
+            AddCurrentForce();
+        }
+    }
+    
+    void OnTriggerEnter(Collider col) 
+    {
+        if (col.gameObject.tag == "Player" || col.gameObject.tag == "Fish")
+        {
+            Rigidbody rb = col.gameObject.GetComponent<Rigidbody>();
+            if (rb != null) 
+            {
+                rbs.Add(rb);
+                empty = false;
+            } 
+        }
+    }
+    
+    void OnTriggerExit(Collider col)
+    {
+        Rigidbody rb = col.gameObject.GetComponent<Rigidbody>();
+        if (rb != null && rbs.Contains(rb)) 
+        {
+            rbs.Remove(rb);
+            if (rbs.Count == 0)
+            {
+                empty = true;
+            } 
+        }
+    }
+    
+    void AddCurrentForce()
+    {
+        foreach (Rigidbody rb in rbs)
+        {
+            if (rb != null) 
+            {
+                rb.AddForce(new Vector3(0f, -1f, 0f) * strength);
+            }
+        }
+    }
+    
+}
