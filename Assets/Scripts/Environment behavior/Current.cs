3a32b06d7f9d194b30b2093bf7896f3ee13512ee
stellarxo <stellajhlee@hotmail.com>
2016-03-04 14:03:14 -0500

    minor fixes

--- Assets/Scripts/Environment behavior/Current.cs
+++ Assets/Scripts/Environment behavior/Current.cs
@@ -2,16 +2,26 @@
 using System.Collections;
 using System.Collections.Generic;
 
+// Acts as an invisible boundary
+// Be mindful of the direction the player will hit the boundary
+// This should be the side of the trigger box collider
 public class Current : MonoBehaviour
 {
-    
+    [Tooltip("The strength of current force.")]
     public float strength;
+    // The direction of the current force.
+    private Vector3 direction;
+    
+    // Whether the current object is empty.
     private bool empty;
+    // Holds all rigidbodies in the current.
     private List<Rigidbody> rbs;
     
     void Start()
     {
         rbs = new List<Rigidbody>();
+        // By default, the current pushes downward.
+        direction = new Vector3(0f, -1f, 0f);
         empty = true;
     }
     
@@ -23,19 +33,18 @@ public class Current : MonoBehaviour
         }
     }
     
+    // Get rigidbody of the colliding game object and add to the list
     void OnTriggerEnter(Collider col) 
     {
-        if (col.gameObject.tag == "Player" || col.gameObject.tag == "Fish")
+        Rigidbody rb = col.gameObject.GetComponent<Rigidbody>();
+        if (rb != null) 
         {
-            Rigidbody rb = col.gameObject.GetComponent<Rigidbody>();
-            if (rb != null) 
-            {
-                rbs.Add(rb);
-                empty = false;
-            } 
-        }
+            rbs.Add(rb);
+            empty = false;
+        } 
     }
     
+    // Remove rigidbody of game object that has exited the current
     void OnTriggerExit(Collider col)
     {
         Rigidbody rb = col.gameObject.GetComponent<Rigidbody>();
@@ -55,7 +64,7 @@ public class Current : MonoBehaviour
         {
             if (rb != null) 
             {
-                rb.AddForce(new Vector3(0f, -1f, 0f) * strength);
+                rb.AddForce(direction * strength);
             }
         }
     }
