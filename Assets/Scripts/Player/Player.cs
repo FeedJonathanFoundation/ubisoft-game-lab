bdc2b92fa48d4159102066c6d66ae0935a7d8700
Alex Ilea <alex@alisterdev.com>
2016-03-08 14:49:36 -0500

    Added smooth color transition

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -60,6 +60,7 @@ public class Player : LightSource
     private bool isDead; // determines is current player is dead
     private new Transform transform;
     private new Rigidbody rigidbody;
+    private MaterialExtensions materials;
     private int currentLevel;
 
     // Use this for initialization
@@ -75,15 +76,10 @@ public class Player : LightSource
 
         this.isDead = false;
         this.currentLevel = SceneManager.GetActiveScene().buildIndex;
+        this.materials = new MaterialExtensions();
         DontDestroyOnLoad(this.gameObject);
-
-        LoadGame();
-        
-        foreach (GameObject probe in GameObject.FindGameObjectsWithTag("Probe"))
-        {
-            MaterialExtensions.SetEmissiveLight(probe, Color.black, 1);
-        }
-        
+        LoadGame();        
+        ChangeProbeColor(Color.black);
     }
     
     void OnLevelWasLoaded(int level) 
@@ -149,20 +145,29 @@ public class Player : LightSource
     {
         if (Input.GetButtonDown("LightToggle") && minimalEnergyRestrictionToggleLights < this.LightEnergy.CurrentEnergy)
         {
-            lightToggle.ToggleLights();            
-            foreach (GameObject probe in GameObject.FindGameObjectsWithTag("Probe"))
+            lightToggle.ToggleLights();
+            if (lightToggle.LightsEnabled)
             {
-                if (lightToggle.LightsEnabled)
-                {
-                    MaterialExtensions.SetEmissiveLight(probe, new Color(1f, 0.3103448f, 0f, 1f), 1);
-                }
-                else 
-                {
-                    MaterialExtensions.SetEmissiveLight(probe, Color.black, 1);   
-                }                    
-            }                           
+                this.ChangeProbeColor(new Color(1f, 0.3103448f, 0f, 1f));
+            }
+            else
+            {
+                this.ChangeProbeColor(Color.black);
+            }                                                 
         }
         lightToggle.LostOfLight(lostOfLightTime, energyCostLightToggle);
+    }
+    
+    private void ChangeProbeColor(Color color)
+    {
+        foreach (GameObject probe in GameObject.FindGameObjectsWithTag("Probe"))
+        {            
+            Renderer renderer = probe.GetComponent<Renderer>();
+            foreach (Material mat in renderer.materials)
+            {
+                StartCoroutine(materials.LerpColor(mat, color, 0.3f));
+            }                    
+        }                    
     }
 
     private void Restart()
