bc0564d3f12d4f21144a43ca73aded16252f2a7e
stellarxo <stellajhlee@hotmail.com>
2016-03-12 14:03:11 -0500

    clean up

--- Assets/Seaweed.cs
+++ Assets/Seaweed.cs
@@ -1,23 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent(typeof(Rigidbody))]
 public class Seaweed : MonoBehaviour
 {
     [SerializeField]
     private float gravity = -9.8f;
 
-    private Rigidbody rb;
+    private Rigidbody rigidBody;
 
     // Use this for initialization
     void Start()
     {
-        rb = gameObject.GetComponent<Rigidbody>();
-        rb.useGravity = false;
+        rigidBody = gameObject.GetComponent<Rigidbody>();
+        rigidBody.useGravity = false;
         ReverseGravity();
     }
     void Update() 
     {
-        rb.velocity += new Vector3(0, gravity * Time.deltaTime, 0);
+        rigidBody.velocity += new Vector3(0, gravity * Time.deltaTime, 0);
     }
     public void ReverseGravity()
     {
