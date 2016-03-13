32b8c6d27ab20d15365255812bc3a3b7ae1b02cc
IAmAllOfYou <k.chiraz93@gmail.com>
2016-03-12 19:44:28 -0500

    refactored code

--- Assets/Scripts/Fish/Jellyfish/JellyfishMovement.cs
+++ Assets/Scripts/Fish/Jellyfish/JellyfishMovement.cs
@@ -12,26 +12,26 @@ public class JellyfishMovement : MonoBehaviour
     private Vector3 movement;
     private Rigidbody parenRigidbody; //make sure the root parent has a rigidbody!!!
 	// Use this for initialization
-	void Start () 
+    void Start () 
     {
-	   jellyfishStart = this.transform.position;
-       goingUp = (Random.value >= 0.5? true : false); //randomly chooses true or false
-       parenRigidbody = this.transform.root.GetComponent<Rigidbody>();
-       SetVolocity();
-	}
-	
-	// Update is called once per frame
-	void Update () 
+        jellyfishStart = this.transform.position;
+        goingUp = (Random.value >= 0.5? true : false); //randomly chooses true or false
+        parenRigidbody = this.transform.root.GetComponent<Rigidbody>();
+        SetVelocity();
+    }
+
+    // Update is called once per frame
+    void Update () 
     {
         float distance = Vector3.Distance(jellyfishStart,this.transform.position);
         if(distance >= jellyfishTravelDistance)
         {
             goingUp = !goingUp;
-            SetVolocity();
+            SetVelocity();
         }
-	}
-    
-    void SetVolocity()
+    }
+
+    void SetVelocity()
     {
         if(goingUp)
         {
@@ -41,7 +41,7 @@ public class JellyfishMovement : MonoBehaviour
         {
             movement = Vector3.down * jellyfishSpeed;
         }
-        parenRigidbody.velocity = Vector3.zero; //rest volocity to zero
+        parenRigidbody.velocity = Vector3.zero; //reset velocity to zero
         parenRigidbody.AddForce(movement * jellyfishSpeed,ForceMode.Force); //set velocity depending on if he is going up or not
     }
 }
