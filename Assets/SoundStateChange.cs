c20f110992455b783db8af3a56b0e46bb87e5703
stellarxo <stellajhlee@hotmail.com>
2016-03-23 00:59:45 -0400

    add state changes + remove player comment - sound

--- Assets/SoundStateChange.cs
+++ Assets/SoundStateChange.cs
@@ -4,22 +4,52 @@ using System.Collections;
 public class SoundStateChange : MonoBehaviour
 {
 
-    private int state = 0;
+    [Tooltip("State of player before he enters")]
+    [SerializeField]
+    private int initialState = 0;
+    protected static int currentState = 0;
 
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            if (state == 0)
+            if (currentState == initialState)
             {
-                AkSoundEngine.SetState("IMAmb1", "Amb1end");
-                state = 1;
+                currentState++;
+                SetState(currentState);
             }
-            else if (state == 1)
+            else if (currentState == (initialState + 1))
             {
+                currentState--;
+                SetState(currentState);
+            }
+        }
+    }
+
+    private void SetState(int newState)
+    {
+        switch (newState)
+        {
+            case 0:
                 AkSoundEngine.SetState("IMAmb1", "Amb1start");
-                state = 0;
-            }
+                break;
+            case 1:
+                AkSoundEngine.SetState("IMAmb1", "Amb1end");
+                break;
+            case 2:
+                AkSoundEngine.SetState("IMAmb2", "CP1");
+                break;
+            case 3:
+                AkSoundEngine.SetState("IMAmb2", "CP2");
+                break;
+            case 4:
+                AkSoundEngine.SetState("IMAmb2", "CP3");
+                break;
+            // case 5:
+                // AkSoundEngine.SetState("IMAmb3", "");
+            default:
+                break;
+            
         }
     }
 }
