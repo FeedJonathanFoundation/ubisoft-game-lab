0eadfe809a6d41269e4286b431cdffcd6c2e6643
Alex Ilea <alex@alisterdev.com>
2016-03-23 00:41:09 -0400

    Smooth light intensity + better player light overall + updates to coroutines + flash player color on light pickup

--- Assets/Scripts/Player/PlayerLightToggle.cs
+++ Assets/Scripts/Player/PlayerLightToggle.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 /// <summary>
 /// PlayerLightToggle class is responsible for behaviour related to player lights.
-/// It toggles the lights on or off and depletes the energy from the Player   
+/// It toggles the lights on or off and depletes the energy from the Player
 ///
 /// @author - Simon T.
 /// @author - Alex I.
@@ -12,38 +12,50 @@ using UnityEngine;
 public class PlayerLightToggle
 {
     // If true, the lights that are children of this object are enabled.
-	private bool lightsEnabled;    
+    private bool lightsEnabled;
     private float timeToDeplete;
     private float minimalEnergyRestriction;
-    private GameObject lightsToToggle; 
+    private GameObject lightsToToggle;
     private LightSource lightSource;
-    
+    private Light closeLight;
+   
     public PlayerLightToggle(GameObject lightsToToggle, bool defaultLightStatus, LightSource lightSource, float minimalEnergy)
-    {        
+    {
         this.lightsToToggle = lightsToToggle;
         this.lightsEnabled = defaultLightStatus;
-        this.lightSource = lightSource;                
+        this.lightSource = lightSource;
         this.minimalEnergyRestriction = minimalEnergy;
         this.ToggleLights();
+        
+        // Cache very close light - needs to be refactored
+        Transform lightTransform = lightsToToggle.transform.Find("VeryClose Light");
+        if (lightTransform != null)
+        {
+            this.closeLight = lightTransform.GetComponent<Light>();
+            this.closeLight.intensity = 0;    
+        }                
     }
 
     /// <summary>
-    /// Changes the status of Light component attached to Player 
+    /// Changes the status of Light component attached to Player
     /// </summary>
     public void ToggleLights()
     {
         foreach (Behaviour childComponent in this.lightsToToggle.GetComponentsInChildren<Light>())
         {
-            childComponent.enabled = !this.lightsEnabled;
+            // Skip VeryClose Light                                     
+            Light light = childComponent.GetComponent<Light>();
+            if (light != null && light.name != "VeryClose Light")
+            {
+                childComponent.enabled = !this.lightsEnabled;
+            }
         }
         // Update the status of the lights
         this.lightsEnabled = !this.lightsEnabled;
-        
-        Debug.Log("Toggle lights");
     }
-    
+
     /// <summary>
-    /// Gradually depletes Player's energy when lights are turned on 
+    /// Gradually depletes Player's energy when lights are turned on
     /// </summary>
     /// <param name="timeToDeplete">Time it takes to deplete a unit of energy when light toggle is on</param>
     /// <param name="energyCost">Energy cost of having Player lights turned on</param>
@@ -71,5 +83,11 @@ public class PlayerLightToggle
     public bool LightsEnabled
     {
         get { return this.lightsEnabled; }
+    }
+
+    public Light ProbeLight
+    {
+        get { return this.closeLight; }
+        set { this.closeLight = value; }
     }
 }
