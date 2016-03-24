0a4be290cd27d187853042e7b23d29a6fc086b39
stellarxo <stellajhlee@hotmail.com>
2016-03-23 23:22:47 -0400

    wwise volcano level

--- Assets/SoundStateChange.cs
+++ Assets/SoundStateChange.cs
@@ -7,10 +7,23 @@ public class SoundStateChange : MonoBehaviour
     [Tooltip("State of player before he enters")]
     [SerializeField]
     private int initialState = 0;
+    [Tooltip("Scene Number, from 0.")]
+    [SerializeField]
+    private int scene = 0;
     [Tooltip("Whether the trigger is a safe zone.")]
     [SerializeField]
     private bool safeZone = false;
     protected static int currentState = 0;
+    
+    void Start()
+    {
+        currentState = scene;
+    }
+    
+    void Update()
+    {
+        Debug.Log("Current State : " + currentState);
+    }
 
     void OnTriggerEnter(Collider other)
     {
@@ -53,22 +66,28 @@ public class SoundStateChange : MonoBehaviour
         {
             case 0:
                 AkSoundEngine.SetState("IMAmb1", "Amb1start");
+                Debug.Log("Amb sound 0");
                 break;
             case 1:
                 AkSoundEngine.SetState("IMAmb1", "Amb1end");
+                Debug.Log("Amb sound 1");
                 break;
             case 2:
-                AkSoundEngine.SetState("IMAmb2", "CP1");
+                AkSoundEngine.SetState("IMAmb2", "CP2");
+                Debug.Log("Amb sound 2");
                 break;
             case 3:
-                AkSoundEngine.SetState("IMAmb2", "CP2");
+                AkSoundEngine.SetState("IMAmb2", "CP3");
+                Debug.Log("Amb sound 3");
                 break;
             case 4:
-                AkSoundEngine.SetState("IMAmb2", "CP3");
+                AkSoundEngine.SetState("IMAmb2", "CP2");
+                Debug.Log("Amb sound 4");
                 break;
-            // case 5:
+            // case 5: // boss theme
                 // AkSoundEngine.SetState("IMAmb3", "");
             default:
+                Debug.Log("Amb sound def");
                 break;
             
         }
