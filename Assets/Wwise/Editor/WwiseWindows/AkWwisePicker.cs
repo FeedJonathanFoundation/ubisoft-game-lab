3416e49a474c65a257baa114274a5aaaffff0b76
stellarxo <stellajhlee@hotmail.com>
2016-03-30 17:32:59 -0400

    wwise

---
+++ Assets/Wwise/Editor/WwiseWindows/AkWwisePicker.cs
@@ -0,0 +1,102 @@
+﻿#if UNITY_EDITOR
+//////////////////////////////////////////////////////////////////////
+//
+// Copyright (c) 2014 Audiokinetic Inc. / All Rights Reserved
+//
+//////////////////////////////////////////////////////////////////////
+
+using UnityEngine;
+using UnityEditor;
+using System;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class Postprocessor : AssetPostprocessor
+{	
+	//This function will be called before script compilation and will save the picker's expantion 
+	static void OnPostprocessAllAssets(	string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths)		
+	{
+		AkWwisePicker.treeView.SaveExpansionStatus ();	
+	}	
+}
+
+
+class AkWwisePicker : EditorWindow
+{
+	public static bool WwiseProjectFound = true;
+	
+    [MenuItem("Window/Wwise Picker", false, (int)AkWwiseWindowOrder.WwisePicker)] 
+    public static void init()
+    {
+		EditorWindow.GetWindow<AkWwisePicker>("Wwise Picker", true, typeof(EditorWindow).Assembly.GetType("UnityEditor.ConsoleWindow"));
+        PopulateTreeview();   
+ 	} 
+
+    public static AkWwiseTreeView treeView = new AkWwiseTreeView();
+
+    void OnEnable()
+    {
+        if (string.IsNullOrEmpty(WwiseSettings.LoadSettings().WwiseProjectPath))
+        {
+            return;
+        }
+        
+        treeView.SaveExpansionStatus();
+        if (AkWwiseWWUBuilder.AutoPopulate())
+            PopulateTreeview();
+    }
+
+    public void OnGUI()
+    {
+        GUILayout.BeginHorizontal("Box");
+        
+        AkWwiseProjectInfo.GetData().autoPopulateEnabled = GUILayout.Toggle(AkWwiseProjectInfo.GetData().autoPopulateEnabled, "Auto populate");
+
+        if (AkWwiseProjectInfo.GetData().autoPopulateEnabled && WwiseProjectFound)
+        {
+            AkWwiseWWUBuilder.StartWWUWatcher();
+        }
+        else
+        {
+            AkWwiseWWUBuilder.StopWWUWatcher();
+        }
+        GUILayout.FlexibleSpace();
+        if (GUILayout.Button("Refresh Project", GUILayout.Width(200)))
+        {
+			treeView.SaveExpansionStatus();
+            if (AkWwiseProjectInfo.Populate())
+                PopulateTreeview(); 
+        }
+        
+        GUILayout.EndHorizontal();
+
+        GUILayout.Space(5);
+
+        treeView.DisplayTreeView(TreeViewControl.DisplayTypes.USE_SCROLL_VIEW);
+
+        if (GUI.changed && WwiseProjectFound)
+        {
+        	EditorUtility.SetDirty(AkWwiseProjectInfo.GetData());             
+        }
+        // TODO: RTP Parameters List
+    }
+
+    //////////////////////////////////////////////////////////
+
+    public static void PopulateTreeview()
+    {	
+        treeView.AssignDefaults(); 
+        treeView.SetRootItem(System.IO.Path.GetFileNameWithoutExtension(WwiseSetupWizard.Settings.WwiseProjectPath), AkWwiseProjectData.WwiseObjectType.PROJECT);
+		treeView.PopulateItem(treeView.RootItem, "Events", AkWwiseProjectInfo.GetData().EventWwu); 
+        treeView.PopulateItem(treeView.RootItem, "Switches", AkWwiseProjectInfo.GetData().SwitchWwu);
+        treeView.PopulateItem(treeView.RootItem, "States", AkWwiseProjectInfo.GetData().StateWwu);
+		treeView.PopulateItem(treeView.RootItem, "SoundBanks", AkWwiseProjectInfo.GetData().BankWwu);
+        treeView.PopulateItem(treeView.RootItem, "Auxiliary Busses", AkWwiseProjectInfo.GetData().AuxBusWwu);
+
+    }
+
+}
+#endif
---
+++ Assets/Wwise/Lumina/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,147 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID AMBIENT1 = 4095159976U;
+        static const AkUniqueID AMBIENT2 = 4095159979U;
+        static const AkUniqueID AMBIENT2STOP = 4142644811U;
+        static const AkUniqueID AMBIENT3 = 4095159978U;
+        static const AkUniqueID BOSSTHEME = 1471715653U;
+        static const AkUniqueID CHECKPOINT = 612075679U;
+        static const AkUniqueID CRITICALHEALTH = 2407075028U;
+        static const AkUniqueID CURRENT = 2037989808U;
+        static const AkUniqueID DEFAULT = 782826392U;
+        static const AkUniqueID DIE = 445985469U;
+        static const AkUniqueID EAT = 781390793U;
+        static const AkUniqueID EXPLOSION = 13776098U;
+        static const AkUniqueID FISH_DETECTION = 3611228611U;
+        static const AkUniqueID FISH_DETECTION_OFF = 2133456085U;
+        static const AkUniqueID FLARE = 1540219311U;
+        static const AkUniqueID FLAREEAT = 3781033855U;
+        static const AkUniqueID JELLYFISHATTACK = 4023935017U;
+        static const AkUniqueID LIGHT_ORB_PICKUP = 4066254940U;
+        static const AkUniqueID LIGHTSTOTOGGLE = 3299248253U;
+        static const AkUniqueID LOWENERGY = 1772229705U;
+        static const AkUniqueID MOVEMENT = 2129636626U;
+        static const AkUniqueID PULSE = 1316273048U;
+        static const AkUniqueID STOPJELLYFISHATTACK = 3799923489U;
+        static const AkUniqueID WALLCRASH = 2901629858U;
+    } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace IMAMB1
+        {
+            static const AkUniqueID GROUP = 1452801642U;
+
+            namespace STATE
+            {
+                static const AkUniqueID AMB1END = 145894011U;
+                static const AkUniqueID AMB1START = 3322567584U;
+            } // namespace STATE
+        } // namespace IMAMB1
+
+        namespace IMAMB2
+        {
+            static const AkUniqueID GROUP = 1452801641U;
+
+            namespace STATE
+            {
+                static const AkUniqueID CP1 = 697796925U;
+                static const AkUniqueID CP2 = 697796926U;
+                static const AkUniqueID CP3 = 697796927U;
+                static const AkUniqueID CP4 = 697796920U;
+            } // namespace STATE
+        } // namespace IMAMB2
+
+        namespace IMAMB3
+        {
+            static const AkUniqueID GROUP = 1452801640U;
+
+            namespace STATE
+            {
+                static const AkUniqueID HIDING = 3538823496U;
+                static const AkUniqueID HUNTED = 671679305U;
+                static const AkUniqueID OVER = 3677122663U;
+            } // namespace STATE
+        } // namespace IMAMB3
+
+        namespace IMAMB4
+        {
+            static const AkUniqueID GROUP = 1452801647U;
+
+            namespace STATE
+            {
+            } // namespace STATE
+        } // namespace IMAMB4
+
+        namespace PLAYERLIFE
+        {
+            static const AkUniqueID GROUP = 444815956U;
+
+            namespace STATE
+            {
+                static const AkUniqueID ALIVE = 655265632U;
+                static const AkUniqueID DEAD = 2044049779U;
+            } // namespace STATE
+        } // namespace PLAYERLIFE
+
+    } // namespace STATES
+
+    namespace SWITCHES
+    {
+        namespace SCENE
+        {
+            static const AkUniqueID GROUP = 1926883983U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID SCENE1 = 1544306476U;
+                static const AkUniqueID SCENE2 = 1544306479U;
+                static const AkUniqueID SCENE3 = 1544306478U;
+                static const AkUniqueID SCENE4 = 1544306473U;
+            } // namespace SWITCH
+        } // namespace SCENE
+
+    } // namespace SWITCHES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID FLAREDISTANCE = 108225470U;
+        static const AkUniqueID LIGHTLEVEL = 827417899U;
+        static const AkUniqueID MASTERVOLUME = 2918011349U;
+        static const AkUniqueID PLAYERVELOCITY = 2886637823U;
+    } // namespace GAME_PARAMETERS
+
+    namespace TRIGGERS
+    {
+        static const AkUniqueID NEW_TRIGGER = 4163741908U;
+    } // namespace TRIGGERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MAIN = 3161908922U;
+        static const AkUniqueID THEMES = 1940854371U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MASTER_SECONDARY_BUS = 805203703U;
+    } // namespace BUSSES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
