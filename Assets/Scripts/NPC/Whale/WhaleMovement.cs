737f9bfeacecac45d48f9efbec073bb6f36a2ed6
stellarxo <stellajhlee@hotmail.com>
2016-03-31 21:49:02 -0400

    Refactor Whale Movement

--- Assets/Scripts/NPC/Whale/WhaleMovement.cs
+++ Assets/Scripts/NPC/Whale/WhaleMovement.cs
@@ -1,48 +1,74 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+/// <summary>
+/// WhaleMovement class is responsible for controlling whale behaviour.
+///
+/// @author - Stella L.
+/// @version - 1.0.0
+///
+/// </summary>
 public class WhaleMovement : MonoBehaviour
 {
 
     [Tooltip("The strength of movement force.")]
     [SerializeField]
     private float movementSpeed;
+    [Tooltip("The frequency of the whale's sine movement.")]
     [SerializeField]
     private float frequency;
+    [Tooltip("The amplitude of the whale's sine movement.")]
     [SerializeField]
     private float magnitude;
-    
+    [Tooltip("The speed of the whale animation.")]
     [SerializeField]
     private float animationSpeed;
-    
     [Tooltip("What the game object moves toward.")]
     [SerializeField]
     private Transform target;
-
+    // Used to calculate sine movement
     private Vector3 axis;
+    // Used to set the whale's next position
     private Vector3 position;
 
-    void Start()
+    /// <summary>
+    /// Initializes the whale movement.
+    /// </summary>
+    protected void Start()
     {
         position = transform.position;
         axis = transform.up;
         Animator animator = GetComponentInParent<Animator>();
         animator.SetFloat("Speed", animationSpeed);
     }
-
-    void Update()
+    
+    /// <summary>
+    /// Applies movement changes every frame.
+    /// </summary>
+    protected void Update()
     {
         Move();
     }
     
-    void Move()
+    /// <summary>
+    /// Calculates the next position of the whale
+    /// based on the target and sine function
+    /// </summary>
+    private void Move()
     {
+        if (target == null)
+        { 
+            Debug.Log("Please set a target for the whale movement.");
+            return;
+        }
         position = Vector3.MoveTowards(transform.position, target.position, movementSpeed * Time.deltaTime);
         transform.position = position + axis * Mathf.Sin(Time.time * frequency) * magnitude;
     }
     
-    // If in contact with a disable collider, disables the game object
-    void OnTriggerEnter(Collider col)
+    /// <summary>
+    /// If in contact with a disable collider, disables the game object.
+    /// </summary>
+    protected void OnTriggerEnter(Collider col)
     {
         gameObject.SetActive(false);
     }
